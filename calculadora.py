fad8804dacfcdb42b4c8556fb39f73331942f858
andreaoa <83839052+andreaoa@users.noreply.github.com>
2023-06-14 11:04:20 -0300

    Projeto
    Adicionando o código em python

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+*****************Calculadora******************
+
+Selecione a opção desejada:
+
+1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão
+0 - Para sair
+
+Digite a operação: 1
+
+Digite o primeiro número: 5
+Digite o segundo número: 10
+5 + 10 = 15
+
+*****************Calculadora******************
+
+Selecione a opção desejada:
+
+1 - Soma
+2 - Subtração
+3 - Multiplicação
+4 - Divisão
+0 - Para sair
+
+Digite a operação: 0
